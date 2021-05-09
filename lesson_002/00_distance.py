a4f3d868ffa79dfdd38ed3abfad611d6b5c0a0c4
Александр <aashchukarev@yandex.ru>
2021-05-09 18:47:18 +0300

    Составлен словарь словарей расстояний между трех столиц, TODO удалена

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -13,10 +13,26 @@ sites = {
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 distances = {}
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+distances['Moscow'] = {}
+distances['London'] = {}
+distances['Paris'] = {}
 
-# TODO здесь заполнение словаря
-
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** 0.5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** 0.5
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** 0.5
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = london_paris
+distances['Paris']['Moscow'] = moscow_paris
+distances['Paris']['London'] = london_paris
 print(distances)
+# Done
+
+
 
 
 
