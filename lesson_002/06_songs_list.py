bbd23f31706f4bd001b5c9bf5616ebe25c0b6b02
Александр <aashchukarev@yandex.ru>
2021-05-09 20:20:03 +0300

    Вычислена длина трех песен из списка и трех песен из словаря в минутах

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -20,7 +20,9 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+all_second = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1]
+all_minute = round(all_second, 0)
+print(f'Три песни звучат {int(all_minute)} минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +40,6 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+all_second = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+all_minute = round(all_second, 0)
+print(f'А другие три песни звучат {int(all_minute)} минут')
