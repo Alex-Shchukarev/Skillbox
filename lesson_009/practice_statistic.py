7e4cd88984a2ece2be16fd1918eddf8d9f996996
Александр <aashchukarev@yandex.ru>
2021-07-02 12:32:32 +0300

    Родительский класс содержит все основные алгоритмя, а дочерний класс применяется для различного вида сортировок результирующей таблицы

--- lesson_009/practice_statistic.py
+++ lesson_009/practice_statistic.py
@@ -1,4 +1,5 @@
 import zipfile
+from operator import itemgetter
 
 
 class CharStat:
@@ -6,6 +7,7 @@ class CharStat:
     def __init__(self, file_name):
         self.file_name = file_name
         self.stat = {}
+        self.stat_convert = []
 
     def unzip_file(self):
         if zipfile.is_zipfile(self.file_name):   # проверка является ли файл zip
@@ -17,14 +19,14 @@ class CharStat:
             print(f'{self.file_name} не является zip-файлом')
 
     def ppt(self):
-        print(self.stat)
+        print(self.stat_convert)
 
-    def collect_for_file(self):
+    def collect_for_file(self):                                       # открываем файл и начинаем построчно читать
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
-                self.collect_for_line(line=line[:-1])
+                self.collect_for_line(line=line[:-1])                 # при чтении отбрасываем перевод строки
 
-    def collect_for_line(self, line):
+    def collect_for_line(self, line):                        # собираем словарь частоты встречаемости символов
         for char in line:
             if char.isalpha():
                 if char in self.stat:
@@ -32,12 +34,39 @@ class CharStat:
                 else:
                     self.stat[char] = 1
 
-    def create_table(self):
-        print(f'')
+    def create_table_rezult(self, marker='frequency', rev=False):              # рисуем результирующую таблицу
+        print('+{elem:-^20}+'.format(elem='+'))
+        print('|{elem1:^9}|{elem2:^10}|'.format(elem1='буква', elem2='частота'))
+        print('+{elem:-^20}+'.format(elem='+'))
+        self.statistic_sort(marker=marker, rev=rev)
+        print('+{elem:-^20}+'.format(elem='+'))
+
+    def statistic_sort(self, marker, rev):                                # сортируем словарь по букве
+        for key, val in sorted(self.stat.items()):
+            print(f'|{key:^9}|{val:^10d}|')
+
+    def convert_dict(self):            # конвертируем словарь в список словарей для применения разных сортировок
+        self.stat_convert = [{'alpha': key, 'frequency': val} for key, val in self.stat.items()]
 
 
+class SortCharStat(CharStat):
 
-char_stat = CharStat('voyna-i-mir.txt.zip')
+    def statistic_sort(self, marker='frequency', rev=False):
+        self.stat_convert = sorted(self.stat_convert, key=itemgetter(marker), reverse=rev)
+        for i in range(len(self.stat_convert)):
+            print('|{elem1:^9}|{elem2:^10d}|'
+                  .format(elem1=self.stat_convert[i]['alpha'], elem2=self.stat_convert[i]['frequency']))
+
+
+# char_stat = CharStat('voyna-i-mir.txt.zip')
+# char_stat.unzip_file()
+# char_stat.collect_for_file()
+# char_stat.create_table_rezult()
+# char_stat.convert_dict()
+# char_stat.ppt()
+
+char_stat = SortCharStat('voyna-i-mir.txt.zip')
 char_stat.unzip_file()
 char_stat.collect_for_file()
-char_stat.ppt()
+char_stat.convert_dict()
+char_stat.create_table_rezult(marker='alpha', rev=True)
