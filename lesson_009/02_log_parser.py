8ce9083099804621c99957cc6abab83e5db520db
Александр <aashchukarev@yandex.ru>
2021-07-02 18:22:20 +0300

    Создан класс ЛогПарсер

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -30,12 +30,33 @@ class LogPars:
 
     def __init__(self, filename):
         self.file_name = filename
+        self.content = {}
 
     def read_file_line(self):
-        pass
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            for line in file:
+                spisok = line.split()
+                self.convert_content(spisok)
 
-    def convert_content(self):
-        pass
+    def convert_content(self, spisok):
+        date, time, req = spisok[0].split('-'), spisok[1].split(':'), spisok[2]
+        self.content['year'], self.content['month'], self.content['day'] = date[0][1:], date[1], date[2]
+        self.content['hour'], self.content['minute'], self.content['second'] = time[0], time[1], time[2][:-1]
+        self.content['req'] = req
+
+    def count_event(self, param='minute', req='NOK'):
+        for i in range(len(self.content)):
+            if self.content[i]['req'] == req:
+                for key in self.content[i]:
+                    if key != param:
+                        print('['+self.content[i][key])
+
+
 
     def write_rezult(self):
         pass
+
+
+lp = LogPars('events.txt')
+lp.read_file_line()
+lp.count_event()
